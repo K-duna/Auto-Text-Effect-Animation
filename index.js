664fbbc0025b569eb15006e1bd54989689f05c57
K-duna <dunaikristina@gmail.com>
2023-12-14 11:19:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const containerEl = document.querySelector(".container");
+
+const careers = ["Business Analyst", "UX Designer", "Frontend Developer", "Procurement specialist", "Internal Auditor ISO 9001:2015"];
+
+let careerIndex = 0;
+
+let characterIndex = 0;
+
+updateText()
+
+function updateText(){
+    characterIndex++
+    containerEl.innerHTML = `
+    <h1>I am ${careers[careerIndex].slice(0,1) === "I" ? "an" : "a"} ${careers[careerIndex].slice(0,characterIndex)}</h1>
+    `;
+
+    if(characterIndex === careers[careerIndex].length) {
+        careerIndex++;
+        characterIndex = 0;
+    }
+    if(careerIndex === careers.length){
+        careerIndex = 0;
+    }
+    setTimeout(updateText,300)
+
+    
+    }
+
